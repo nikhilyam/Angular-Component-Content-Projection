7d6cfe72fd8356be6336456fba3adef397c40fb6
nikhilyam <nik.yamsalwar@gmail.com>
2022-02-11 08:17:48 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component, Directive } from '@angular/core';
+import {
+  Component,
+  Directive,
+  Input,
+  ContentChild,
+  TemplateRef,
+  HostBinding,
+  HostListener,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,14 +14,32 @@ import { Component, Directive } from '@angular/core';
 })
 export class AppComponent {}
 
+@Directive({
+  selector: 'button[appExampleZippyToggle]',
+})
+export class ZippyToggelDirective {
+  @HostBinding('attr.aria-expanded') areaExpanded = this.zippy.expanded;
+  @HostBinding('attr.area-controls') areControls = this.zippy.contentId;
+  @HostListener('click') toggleZippy() {
+    this.zippy.expanded = !this.zippy.expanded;
+  }
+  constructor(public zippy: ZippyComponent) {}
+}
+
+@Directive({
+  selector: '[appExampleZippyContent]',
+})
+export class ZippyContentDirective {
+  constructor(public templateRef: TemplateRef<unknown>) {}
+}
+
+let nextId = 0;
 @Component({
   selector: 'app-example-zippy',
   templateUrl: './example-zippy.template.html',
 })
-export class ZippyComponent {}
-
-@Directive({})
-export class ZippyContentDirective {}
-
-@Directive({})
-export class ZippyToggelDirective {}
+export class ZippyComponent {
+  contentId = `zippy-${nextId++}`;
+  @Input() expanded = false;
+  @ContentChild(ZippyContentDirective) content!: ZippyContentDirective;
+}
