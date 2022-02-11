67d9406f8be35f3a0d053be6b519f09494c91dd7
nikhilyam <nik.yamsalwar@gmail.com>
2022-02-11 07:27:37 +0530

    5 files modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,6 @@
-import { Component, VERSION } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
-})
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
-}
+  selector: 'app-root',
+  template: `<h1> Hi NIkhil Welcome to World</h1>`;
+}) export class AppComponent {}
