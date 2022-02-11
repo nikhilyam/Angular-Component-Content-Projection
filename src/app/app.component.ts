92758ea1713e852c29f450154486dcaf67f71588
nikhilyam <nik.yamsalwar@gmail.com>
2022-02-11 07:28:57 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1> Hi NIkhil Welcome to World</h1>`;
-}) export class AppComponent {}
+  template: `<h1> Hi NIkhil Welcome to World</h1>`,
+})
+export class AppComponent {}
